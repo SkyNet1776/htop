86e161ed1194e6b967841ecf4f38f4f238fe9d11
SkyNet1776 <46215515+SkyNet1776@users.noreply.github.com>
2019-01-07 21:21:48 +0000

    Update ColorsPanel.h

--- ColorsPanel.h
+++ ColorsPanel.h
@@ -27,8 +27,6 @@ typedef struct ColorsPanel_ {
 } ColorsPanel;
 
 
-const char *ColorSchemeNames[999]; //MAKE THIS DYNAMIC LATER! 
-
 extern PanelClass ColorsPanel_class;
 
 ColorsPanel* ColorsPanel_new(Settings* settings, ScreenManager* scr);
