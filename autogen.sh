d828a8dbf808089ebeac0a3b5648430e2657c08e
SkyNet1776 <46215515+SkyNet1776@users.noreply.github.com>
2019-01-14 23:37:13 +0000

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p m4
 autoreconf --install --force
-cp Office.theme ~/.config/htop/Office.theme
-cp Classic.theme ~/.config/htop/Classic.theme
+cp Office.theme ~/.config/htop/theme-tools/Office.theme
+cp Classic.theme ~/.config/htop/theme=tools/Classic.theme
