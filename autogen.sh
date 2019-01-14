ef99304d0cbf743966ffc93c9496fc503f3832d7
SkyNet1776 <46215515+SkyNet1776@users.noreply.github.com>
2019-01-14 23:39:18 +0000

    im an idiot

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p m4
 autoreconf --install --force
-cp Office.theme ~/.config/htop/theme-tools/Office.theme
-cp Classic.theme ~/.config/htop/theme=tools/Classic.theme
+cp theme-tools/Office.theme ~/.config/htop/Office.theme
+cp theme-tools/Classic.theme ~/.config/htop/Classic.theme
