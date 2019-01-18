1ba046d7a39394691bb9d2b3748d72d36a11b197
SkyNet1776 <46215515+SkyNet1776@users.noreply.github.com>
2019-01-17 21:54:44 -0800

    bandaid
    first time htop user's autogen.sh will error because there isn't an htop folder to put themes in. this is a temporary fix to show the proof of concept.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 mkdir -p m4
 autoreconf --install --force
+mkdir ~/.config/htop  #temporary bandaid for including themes
 cp theme-tools/Office.theme ~/.config/htop/Office.theme
 cp theme-tools/Classic.theme ~/.config/htop/Classic.theme
